updated to correct variance calculator

sim/python/scripts/PriceUpdates.py
@@ -9,7 +9,7 @@ from matplotlib.animation import FuncAnimation
 
 tr = [14, 34]
 time = np.arange(tr[0], tr[1]+1)
-seed_ = 2000
+seed_ = 1999
 try:
     os.mkdir("pics")
 except:
@@ -24,7 +24,7 @@ os.mkdir(seedpath)
 
 np.random.seed(seed_)
 
-gmm = dataAggregator.makeModel(timeRange=tr)
+gmm = dataAggregator.makeModel(timeRange=tr,mixtures=[5])
 
 eGMM = energyGMM(gmm)
 MLE = eGMM.mle()
@@ -84,6 +84,7 @@ def update(i):
     ax.fill_between(time, MLE2 - STD2, MLE2 + STD2, alpha=0.2, color='b')
     return line1, ax
 
+# must add custom init function, otherwise FuncAnimation will do first index twice
 def init():
     pass
 

sim/python/src/energyGMM.py
@@ -65,10 +65,16 @@ class energyGMM(object):
     
     def std(self):
         """Returns the representative standard deviation of the maximum 
-        likelihood estimate of the GMM"""
+        likelihood estimate of the GMM
+        
+        Formula adapted from
+        https://stats.stackexchange.com/questions/16608/what-is-the-variance-of-the-weighted-mixture-of-two-gaussians
+        """
 
         weighted_cov = self.covariances_.T.dot(self.weights_)
-        return np.sqrt(np.diag(weighted_cov))
+        mean_contrib = (self.means_**2).T.dot(self.weights_) - self.mle()**2
+        variance = np.maximum(0,np.diag(weighted_cov)+mean_contrib)
+        return np.sqrt(variance)
 
     def posterior_std(self):
         """Returns the representative standard deviation of the maximum
@@ -76,8 +82,11 @@ class energyGMM(object):
 
         weighted_cov = self.posterior_covariances.T.dot(
             self.posterior_weights)
-        return np.sqrt(np.diag(weighted_cov))
-    
+        mean_contrib = (self.posterior_means**2).T.dot(self.posterior_weights) \
+            - self.posterior_mle()**2
+        variance = np.maximum(0,np.diag(weighted_cov)+mean_contrib)
+        return np.sqrt(variance)
+
     def sample(self,n_samples=1,seed=None):
         """Returns n_samples samples from the current distribution. Returns a 
         numpy array of size (n_samples,time_length)"""