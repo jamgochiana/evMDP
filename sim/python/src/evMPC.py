76555f773fd34c83862a802b944762a1bb9ee530
Arec Jamgochian <jamgochian95@gmail.com>
2019-04-13 18:30:39 -0700

    adding functions to output past and future charging ratios and total demands

--- sim/python/src/evMPC.py
+++ sim/python/src/evMPC.py
@@ -12,16 +12,16 @@ class evMPC(object):
     cvxpy-style costs and constraints to pass to a solver
 
     Attributes:
-        state - current state of charge
-        demand_model - energyGMM-class base demand model prior/posterior
+        state: current state of charge
+        demand_model: energyGMM-class base demand model prior/posterior
 
-        end_times - time of departure for each car
-        rise - per car electric rise
-        eta - dual weighting parameter (applied to electric costs)
+        end_times: time of departure for each car
+        rise: per car electric rise
+        eta: dual weighting parameter (applied to electric costs)
 
-        past_times - (# timesteps, )-sized numpy array of already visited
+        past_times: (# timesteps, )-sized numpy array of already visited
             simulation times
-        actions_taken - (# cars, # timesteps)-sized numpy array of actions
+        actions_taken: (# cars, # timesteps)-sized numpy array of actions
             taken so far
             
 
@@ -46,11 +46,11 @@ class evMPC(object):
         
         Args:
             
-            t - initial time for simulation
-            cars - number of cars in the simulation
-            charge - initial charge state - can be single value or array of 
+            t: initial time for simulation
+            cars: number of cars in the simulation
+            charge: initial charge state - can be single value or array of 
                 length cars
-            charge_rate - 
+            charge_rate: 
 
         Raises:
             ValueError if charge or chargeRate do not have length cars
@@ -66,18 +66,20 @@ class evMPC(object):
         self.eta = eta 
         
         self.past_times = np.empty((0,), float)
-        self.actions_taken = np.empty((cars,0))
+        self.actions_taken = np.empty((cars,0), int)
+        
+        self.times_remaining = np.arange(t, end_times.max())
+        self.actions_remaining = np.zeros((cars,len(self.times_remaining))
 
     def step(self, observations, dt, relaxation=False, solver=cp.GUROBI):
         """Perform MPC up through the next observed base demand
         
         Args:
-            
-            observations - numpy array or float of new hourly observations
-            dt - time step over which to perform actions
-            relaxation - whether or not to solve the relaxed problem
+            observations: numpy array or float of new hourly observations
+            dt: time step over which to perform actions
+            relaxation: whether or not to solve the relaxed problem
                 (default: False)
-            solver - cvxpy-compatible solver to solve optimization problem
+            solver: cvxpy-compatible solver to solve optimization problem
                 (default: cp.GUROBI)
 
         Raises:
@@ -128,3 +130,47 @@ class evMPC(object):
         
     def simulate(self):
         """Simulate MPC through end times"""
+        pass
+    
+    def total_demand(self):
+        """Return both past and future estimated total demand
+        
+        Returns:
+            past: (2, # past timesteps)-sized array with the first row w/
+                past timesteps, and the second row w/ total past demand
+            future: (2, # future timesteps)-sized array with the first row
+                w/ future timesteps, and the second row w/ total future demand
+        """
+
+        past_base_demand = np.interp(self.past_times,
+            self.demand_model.times, self.demand_model.posterior_mle())
+        future_base_demand = np.interp(self.times_remaining,
+            self.demand_model.times, self.demand_model.posterior_mle())
+
+        past_total_demand = past_base_demand+self.rise*self.actions_taken.sum(
+            axis=0)
+        future_total_demand = future_base_demand+self.rise \
+            * self.actions_remaining.sum(axis=0)
+
+        past = np.vstack((self.past_times, past_total_demand))
+        future = np.vstack((self.times_remaining,future_total_demand))
+        return past, future
+
+    def charging_ratio(self):
+        """Return both past and future estimated ratio of cars charging
+        
+        Returns:
+            past: (2, # past timesteps)-sized array with the first row w/
+                past timesteps, and the second row w/ ratio of cars charging
+            future: (2, # future timesteps)-sized array with the first row
+                w/ future timesteps, and the second row w/ ratio of cars charging
+        """
+        
+        past_ratio = self.actions_taken.sum(
+            axis=0) / self.actions_taken.shape[0]
+        future_ratio = self.actions_remaining.sum(
+            axis=0) / self.actions_remaining.shape[0]
+        
+        past = np.vstack((self.past_times, past_ratio))
+        future = np.vstack((self.times_remaining,future_ratio))
+        return past, future
